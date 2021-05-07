f485da19bf336baa24a5a117562341a3e856a7cf
Stephen Lance <80191260+StepPen-codes@users.noreply.github.com>
2021-05-08 01:01:50 +0800

    Update dropdown-class.js

--- javascript/dropdown-class.js
+++ javascript/dropdown-class.js
@@ -5,14 +5,26 @@ class dropDown extends HTMLElement{
     
     this.#template.innerHTML = 
       `<style>
-        
+        .drop-down-container{
+          display:inline-block;
+        }
+        .menu{
+          width:100%;
+          height:
+        }
+        input{
+          width:100%;
+        }
       </style>
-      <input type='text'><div class="menu"></div>`
+      <div class='container'>
+        <input class='input-field' type='text'>
+        <div class="menu"></div>
+      </div>`
     
     this.attachShadow({mode: 'open'})
     this.shadowRoot.appendChild(this.#template.content.cloneNode(true))
     if (this.hasAttribute('placeholder'))
-      this.shadowRoot.querySelector('input').setAttribute('placeholder', this.getAttribute('placeholder'))
+      this.shadowRoot.querySelector('input').setAttribute('placeholder',               this.getAttribute('placeholder'))
   }
 }
 
