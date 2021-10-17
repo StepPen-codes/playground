76cd69cdd5bb708324631559e76ad2b42439bd17
Stephen Lance <80191260+StepPen-codes@users.noreply.github.com>
2021-10-17 18:27:16 +0800

    Create main.js

---
+++ homepage/main.js
@@ -0,0 +1,12 @@
+let local = "homepage";
+
+const dashboard = new Dashboard($("#menu"), $("#editor"), [
+  {
+    url: "https://google.com",
+    name: "Google",
+    imgurl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png"
+  }
+]);
+
+getlocal();
